4c380a4a07d5db43e52107bc4c7c899e7aa4da38
Isaac Hernandez <isaachernandez990@gmail.com>
2023-04-02 21:37:49 -0600

    done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+ if (someValue < 400)
+return "This one is on me!"
+  
+else if (someValue <= 2000)
+
+return "That will be twenty bucks."
+
+else if (someValue <= 2500)
+ return "I will gladly take your thirty bucks."
+
+ else (someValue > 2500)
+return "No can do."
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city) {
   // Write your code here!
+
+let response = city == "NYC" ? `Ok, sounds good.` : `No go.`
+  return response
+}
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+switch (tip) {
+  case "generous":
+    return ("Thank you so much.");
+  case "not as generous":
+    return ("Thank you.");
+  case "thanks for everything":
+    return ("Bye.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
+
